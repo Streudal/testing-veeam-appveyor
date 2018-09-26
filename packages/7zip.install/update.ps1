02498d1f5bc59a662f0324b4217f6bb479c78d41
tastreu <42149626+tastreu@users.noreply.github.com>
2018-09-26 12:14:11 -0400

    Update update.ps1

--- packages/7zip.install/update.ps1
+++ packages/7zip.install/update.ps1
@@ -1,36 +1,55 @@
 import-module au
 
-$url                 = 'https://forums.veeam.com/veeam-agent-for-windows-f33/current-version-t29537.html'
+$url                 = 'https://www.7-zip.org/'
 $checksumTypeZip     = "SHA512"
 
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1'   = @{
-            "(^\s*[$]*url\s*=\s*)('.*')"                 = "`$1'$($Latest.URL64)'"
-            "(^\s*[$]*checksumZip\s*=\s*)('.*')"         = "`$1'$($Latest.Checksum64)'"
-            "(^\s*[$]*checksumTypeZip\s*=\s*)('.*')"     = "`$1'$($Latest.ChecksumType64)'"
+            "(^\s*[$]*url\s*=\s*)('.*')"                 = "`$1'$($Latest.URL)'"
+            "(^\s*[$]*checksum\s*=\s*)('.*')"         = "`$1'$($Latest.Checksum)'"
+            "(^\s*[$]*checksumType\s*=\s*)('.*')"     = "`$1'$($Latest.ChecksumType)'"
         };
     }
+    @{
+        'tools\chocolateyInstall.ps1'   = @{
+            "(^\s*[$]*url64\s*=\s*)('.*')"                 = "`$1'$($Latest.URL64)'"
+            "(^\s*[$]*checksum64\s*=\s*)('.*')"         = "`$1'$($Latest.Checksum64)'"
+            "(^\s*[$]*checksumType64\s*=\s*)('.*')"     = "`$1'$($Latest.ChecksumType64)'"
+        };
 }
 
 function global:au_GetLatest {
+    
     $download_page = Invoke-WebRequest -Uri $url -UseBasicParsing -DisableKeepAlive
 
     $reLatestbuild = "(.*7-Zip.*)"
     $download_page.RawContent -imatch $reLatestbuild
-    $latestbuild = $Matches[1]
+    $latestbuild = $Matches[0]
 
     $reVersion = "\bv?[0-9]+\.[0-9]+\.[0-9]+(?:\.[0-9]+)(?:\.[0-9]+)?\b"
     $latestbuild -imatch $reVersion
-    $version = $Matches[1]
-
+    $version = $Matches[0]
+    
+    If (Get-ProcessorBits -eq 64){
     $urlPackage = "https://7-zip.org/a/7z$($version)-x64.exe"
-
-    return @{
+    
+        return @{
         URL64                 = $urlPackage;
-        ChecksumType64        = $checksumTypeZip;
+        ChecksumType64        = $checksumType;
         Version               = $version
+        }
     }
+    else {
+    $urlPackage = "https://7-zip.org/a/7z$($version).exe"
+        return @{
+        URL                 = $urlPackage;
+        ChecksumType        = $checksumType;
+        Version             = $version
+        }
+    }
+
+
 }
 
 function global:au_AfterUpdate ($Package) {
