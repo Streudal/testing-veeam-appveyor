35b7fc9b227618d41d037506b32f6d861ebcf970
tastreu <42149626+tastreu@users.noreply.github.com>
2018-10-09 16:45:11 -0400

    Update update_all.ps1

--- update_all.ps1
+++ update_all.ps1
@@ -107,7 +107,7 @@ $Options = [ordered]@{
 
 if ($ForcedPackages) { Write-Host "FORCED PACKAGES: $ForcedPackages" }
 $global:au_Root         = $Root          #Path to the AU packages
-$global:au_GalleryUrl   = ''             #URL to package gallery, leave empty for Chocolatey Gallery
+$global:au_GalleryUrl   = ''             #URL to package gallery, leave empty for Chocolatey Gallery/or if au_gallery is changed to private/public repo then leave blank as well.
 $global:info = updateall -Name $Name -Options $Options
 
 #Uncomment to fail the build on AppVeyor on any package error
